1eaaabb1bee6eda838ddb630acd249c847a67472
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-06-07 08:56:08 +0400

    Update and rename models_bd.py to employee.py

--- application/db/employee.py
+++ application/db/employee.py
@@ -1,17 +1,14 @@
 import sqlalchemy as sa
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
-Base = declarative_base()
 
-class People(Base):
-    __tablename__ = 'people'
+class Employee(BaseModel):
+    __tablename__ = 'employees'
 
-    id = sa.Column(sa.Integer, primary_key=True)
     first_name = sa.Column(sa.String(length=40))
     last_name = sa.Column(sa.String(length=40))
     date_birth = sa.Column(sa.Date)
-
-class Employees(Base):
-    id = sa.Column(sa.Integer, primary_key=True)
-
-
+    
+    def __repr__(self):
+        res = '{} {}'.format(self.first_name, self.last_name)
+        return res
